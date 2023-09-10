5aaa65e5b87804c5c976e23f974d9c8ba7f2963a
Lizhouyi <liulang@eeho.cn>
2023-09-10 15:35:15 +0800

    没有改动

--- modules/sqlite_server.py
+++ modules/sqlite_server.py
@@ -8,7 +8,6 @@ engine = create_engine('sqlite:///speedtest.db')
 Base = declarative_base()
 cst = pytz.timezone('Asia/Shanghai')
 
-
 # 定义数据模型类
 class SpeedtestServer(Base):
     __tablename__ = 'speedtest_servers'
@@ -29,7 +28,6 @@ class SpeedtestServer(Base):
     show = Column(Integer)
     internal = Column(Integer)
 
-
 class SpeedtestHistory(Base):
     __tablename__ = 'speedtest_history'
 
@@ -49,29 +47,3 @@ class SpeedtestHistory(Base):
 if __name__ == '__main__':
     # 创建表格
     Base.metadata.create_all(engine)
-
-    '''
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # 添加Server服务器
-    server_info = SpeedtestServer(
-        sponsor="江苏移动 5G",
-        name="镇江",
-        host="5gzhenjiang.speedtest.jsinfo.net.prod.hosts.ooklaserver.net:8080",
-        url="http://5gzhenjiang.speedtest.jsinfo.net:8080/speedtest/upload.php",
-        country="China",
-        cc="CN",
-        lat=0.0,
-        lon=0.0,
-        distance=0,
-        preferred=0,
-        https_functional=1,
-        force_ping_select=1,
-        show=1,
-        internal=0
-    )
-
-    session.add(server_info)
-    session.commit()
-    '''
